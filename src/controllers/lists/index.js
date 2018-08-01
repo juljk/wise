1b7ab0e1a3e53fe1bf20e2ca12be6a9d32d593cb
juljk <julikjulik22@gmail.com>
2018-08-02 01:04:31 +0300

    add client side in folder

--- src/controllers/lists/index.js
+++ src/controllers/lists/index.js
@@ -1,18 +1,14 @@
 const { Router: router } = require('express');
 
-const { get } = require('./get');
 const { list } = require('./list');
 const { create } = require('./create');
-const { update } = require('./update');
 const { remove } = require('./remove');
 
 module.exports = (models) => {
   const api = router();
 
-  api.get('/', list(models));
-  api.get('/:_id', get(models));
-  api.post('/', create(models));
-  api.patch('/:_id', update(models));
+  api.get('/', list(models));  
+  api.post('/', create(models));  
   api.delete('/:_id', remove(models));
   
   return api;
